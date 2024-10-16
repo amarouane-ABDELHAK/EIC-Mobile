50bb3ca249b22d5e7b2f8e0d03cbad0ac1db5ff7
Abdelhak Marouane <am0089@uah.edu>
2024-10-16 13:58:14 -0500

    Adding Digital Analytics Program (DAP) header to the app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-dap-script',
+      transformIndexHtml(html) {
+        // Check if the environment variable is set to 'true'
+        if (process.env.VITE_ADD_DAP_SCRIPT === 'true') {
+          return html.replace(
+            '</head>',
+            '<script async="true" type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA&amp;subagency=HQ" id="_fed_an_ua_tag"></script></head>'
+          );
+        }
+        return html;
+      }
+    }
+  
+  ],
 })
