c15aa80acb76173056178660ed1ffb5c3da4533e
Gjore Milevski <dzole0311@gmail.com>
2024-10-17 11:26:25 +0200

    Update name of env variable

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
         name: 'inject-dap-script',
         transformIndexHtml(html) {
           // Check if the environment variable is set to 'true'
-          if (env.VITE_ADD_DAP_SCRIPT === 'true') {
+          if (env.ADD_DAP_SCRIPT === 'true') {
             return html.replace(
               '</head>',
               '<script async="true" type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA&amp;subagency=HQ" id="_fed_an_ua_tag"></script></head>'
